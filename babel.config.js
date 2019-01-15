90ac2aa47c152118c532510af7827697ca7f2cfd
Konstantin <elizabethkwekjl@crimsonlogic.com>
2019-01-15 18:05:17 +0800

    init

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   presets: [
-    '@vue/app'
+    ['@vue/app', {
+      useBuiltIns: 'entry',
+      polyfills: [
+        'es6.promise', 'es7.promise.finally',
+        'es6.symbol',
+        // 'es6.array.iterator', 'es6.array.from', 'es6.array.includes',
+        'es6.object.assign'
+      ]
+    }]
+  ],
+  plugins: [
+    "@babel/plugin-proposal-object-rest-spread"
   ]
 }
