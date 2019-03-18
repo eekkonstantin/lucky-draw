91b1c57a6d06ddd72df5fc3a72f06ce308c29b91
Konstantin K <eekkonstantin@gmail.com>
2019-03-18 22:34:32 +0800

    Removed logging

--- src/components/Launch.vue
+++ src/components/Launch.vue
@@ -118,12 +118,10 @@ export default {
   },
   beforeMount() {
     // this.prize = Array.from(this.$store.state.prizes).pop()
-    console.log("beforeMount");
     let check = this.$store.state.prizes
     if (!check[check.length - 1].wonBy)
       this.prizeID = -1
     else {
-      console.log("prize");
       for (let i=check.length-1; i>-1; i--) {
         if (!check[i].wonBy) {
           this.prizeID = i
